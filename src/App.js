be04804cbfa49306b2baf749af257c65dec0c92a
Muath <Muath.khalifa@yahoo.com>
2022-10-02 03:12:39 +0100

    added delete button to delete blogs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './create';
 import BlogDetails from './blogdetails';
+import NotFound from './notfound';
 
 function App() {
 
@@ -22,6 +23,9 @@ function App() {
           </Route>
           <Route path="/blogs/:id">
             <BlogDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
           </Switch>
         </div>
--- src/create.js
+++ src/create.js
@@ -1,15 +1,34 @@
 import { useState } from "react";
-
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 const Create = () => {
 
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [author, setAuthor] = useState('mario')
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = {title, body, author};
+
+        setIsPending(true);
+
+        fetch("http://localhost:8000/blogs",{
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log("new blog added")
+            setIsPending(false);
+            history.push('/')
+        });
+    }
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title: </label>
                 <input
                     type="text"
@@ -33,8 +52,8 @@ const Create = () => {
                     <option value="mario">Mario</option>
                     <option value="Yoshi">Yoshi</option>
                 </select>
-                <button>Add Blog</button>
-                {/* <p>{author}</p> */}
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button>Add Blog...</button>}
             </form>
         </div>
      );
